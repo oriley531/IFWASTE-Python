554ebb9d6bfb6450fa945ad9f37733a06f4c079a
oriley531 <rorr.prof@gmail.com>
2023-07-07 12:17:35 -0400

    Began eat()

--- IFWASTE_0-0.py
+++ IFWASTE_0-0.py
@@ -5,6 +5,7 @@ class Food:
     def __init__(self, food_type, amount_kg):
         self.type = food_type
         self.amount_kg = amount_kg
+        self.expirationTime = random.uniform(1,5)
 
 
 class House:
@@ -13,12 +14,29 @@ class House:
         self.members_dependent = random.randint(0, 3)
         self.menu = []
 
-    def generate_menu(self):
+    def shop(self):
+        foods = [
+            "Meat & Fish",
+            "Dairy & Eggs",
+            "Fruits and Vegetables",
+            "Baked Goods",
+            "Dry Foods",
+            "Snacks, Condiments, & Other",
+            "Liquids/Oils/Grease", 
+            "Cooked/Prepared Items/Leftovers", 
+        ]
         for i in range(self.members_adult):
             food_type = random.choice(foods)
             amount_kg = round(random.uniform(1, 5), 2)
             food = Food(food_type, amount_kg)
             self.menu.append(food)
+    
+
+    def eat(self) :
+        for i in range(self.members_adult):
+            food = random.choice(menu)
+            food.amount_kg -= random.uniform(0.3, 0.7)
+
 
     def print_menu(self):
         for food in self.menu:
