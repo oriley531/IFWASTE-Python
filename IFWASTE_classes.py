4f62d16acab3a37047cedb3a6e733782ccceba5d
oriley531 <rorr.prof@gmail.com>
2023-07-09 16:34:03 -0400

    Added kcal per kg and decay functions

--- IFWASTE_classes.py
+++ IFWASTE_classes.py
@@ -5,7 +5,31 @@ class Food:
     def __init__(self, foodType, amount_kg):
         self.type = foodType
         self.amount_kg = amount_kg
-        self.expirationTime = random.uniform(1,5)
+        self.expirationTime = random.uniform(6,14)
+
+    def __init__(self, foodType, amount_kg):
+        self.type = foodType
+        self.amount_kg = amount_kg
+        if self.type == "Dairy & Eggs":
+            self.kcal_per_kg = 10*(random.gauss(182, 112)+random.gauss(171,74))/2 #averages the random odds between dairy and egg products
+        elif self.type == "Meat & Fish":
+            self.kcal_per_kg = 10*random.gauss(224, 98)
+        elif self.type == "Dry Foods":
+            self.kcal_per_kg = 10*(random.gauss(337, 217)+random.gauss(337, 110))/2 # averages the random odds between beans and grains
+        elif self.type == "Fruits and Vegetables":
+            self.kcal_per_kg = 10*(random.gauss(67,56)+random.gauss(83, 80))/2 # Averages the random odds between Fruits and Vegetables
+        elif self.type == "Liquids/Oils/Grease":
+            self.kcal_per_kg = 10*random.gauss(390, 226) # taking data from "Fats, oils, and salad dressings" data -> assuming close
+        elif self.type == "Snacks, Condiments, & Other":
+            self.kcal_per_kg = 10*random.gauss(242, 190) # Assumed to be equal too "Snacks, Condiments, & Other"
+        elif self.type == "Cooked/Prepared Items/Leftovers":
+            self.kcal_per_kg = random.gauss(1300, 200) #assumed ready to eat or frozen from the store
+            self.expiration_time = 7
+        else :
+            raise ValueError("Not a listed Food Class")
+
+    def decay(self):
+        self.expiration_time -= 1
 
     def __str__(self): 
         return(f"Food: {self.type}, Amount: {self.amount_kg} kg")
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 - Every meal is with whole house\
 - Food has no inedible parts\
+- Data on kcal taken taken and interpreted from FNDDS data through (Drewnowski, 2010)\
